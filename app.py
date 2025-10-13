dfed81459c70e528d09d74921a0c06490edfcb5f
Gustavo Henrick <gustavo.hrsilva15@gmail.com>
2025-10-13 14:55:24 -0300

    feat: Renderiza o template HTML inicial na rota principal

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template # Importe a nova função
 
-# 1. Cria a nossa aplicação web
 app = Flask(__name__)
 
-# 2. Define a rota (URL) para a página inicial
-# O decorador '@app.route("/")' diz ao Flask: "Quando alguém acessar a URL principal do site ('/')...
 @app.route("/")
 def home():
-    # 3. ...execute esta função. E o que ela retornar será exibido no navegador."
-    return "Olá, Mundo! Este é o nosso servidor DevFinder."
+    # A mágica acontece aqui: renderizamos nosso arquivo HTML
+    return render_template("index.html")
 
-# O código abaixo é necessário para rodar a aplicação diretamente do terminal
-# Apenas para o ambiente de desenvolvimento.
 if __name__ == "__main__":
     app.run(debug=True)
